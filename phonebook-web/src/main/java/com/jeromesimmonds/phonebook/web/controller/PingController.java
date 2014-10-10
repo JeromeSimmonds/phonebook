0f7fe0a39ada4b10b512857b35b244076f23dc2a
JeromeSimmonds <jerome.simmonds@gmail.com>
2014-10-10 03:03:42 +0200

    Fix

--- phonebook-web/src/main/java/com/jeromesimmonds/phonebook/web/controller/PingController.java
+++ phonebook-web/src/main/java/com/jeromesimmonds/phonebook/web/controller/PingController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -16,6 +17,7 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/ping")
 public class PingController {
 
+	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView ping(HttpServletResponse response) throws Exception {
 		response.getWriter().println(new Date());
 		return null;
