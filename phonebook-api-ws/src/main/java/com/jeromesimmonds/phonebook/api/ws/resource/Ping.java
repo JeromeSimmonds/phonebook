7b7a1e53fa49a6ad6cd1fec194a3dc9355f1175a
JeromeSimmonds <jerome.simmonds@gmail.com>
2014-10-16 00:32:55 +0200

    Update

--- phonebook-api-ws/src/main/java/com/jeromesimmonds/phonebook/api/ws/resource/Ping.java
+++ phonebook-api-ws/src/main/java/com/jeromesimmonds/phonebook/api/ws/resource/Ping.java
@@ -7,7 +7,11 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
- 
+
+/**
+ * @author Jerome Simmonds
+ *
+ */
 @Path("/ping")
 public class Ping {
 	
