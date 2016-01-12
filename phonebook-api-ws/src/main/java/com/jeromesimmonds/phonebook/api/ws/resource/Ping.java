190f8558ec9e9410b1653278f76b60d2d116a3e7
Jerome S <jerome.simmonds@gmail.com>
2016-01-11 23:03:21 -0500

    @PermitAll

--- phonebook-api-ws/src/main/java/com/jeromesimmonds/phonebook/api/ws/resource/Ping.java
+++ phonebook-api-ws/src/main/java/com/jeromesimmonds/phonebook/api/ws/resource/Ping.java
@@ -2,6 +2,7 @@ package com.jeromesimmonds.phonebook.api.ws.resource;
 
 import java.util.Date;
 
+import javax.annotation.security.PermitAll;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -13,6 +14,7 @@ import javax.ws.rs.core.Response;
  *
  */
 @Path("/ping")
+@PermitAll
 public class Ping {
 	
 	@GET
